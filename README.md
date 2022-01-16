7e14d9092d99a3b4e9c8f339533b43ffc800298f
Darsheax <darsheax.program@gmail.com>
2022-01-16 13:12:20 +0100

    New feature: image resizing
    Image resizing + new settings image for sizes

---
+++ README.md
@@ -0,0 +1,8 @@
+# STARTER KIT GULP BUNDLE
+
+## Installation
+
+> npm install
+
+> For image manipulation go on : https://imagemagick.org/script/download.php & Check ✅ **"Install legacy utilities"**
+
--- gulpconfig/tasks/image.js
+++ gulpconfig/tasks/image.js
@@ -3,7 +3,9 @@
 /==================*/
 import  {dest, src}         from "gulp";
 import  imagemin            from "gulp-imagemin";
+import  imageResize         from "gulp-image-resize";
 import  imageminPngquant    from "imagemin-pngquant";
+import  rename              from "gulp-rename";
 
 /*==================/
   SETTINGS
@@ -11,12 +13,33 @@ import  imageminPngquant    from "imagemin-pngquant";
 const   imgDIST             = "assets/images"
 const   imgOUTPUT           = "prod/images/"
 
+const   imgSIZES            =   [
+                                    {width: 360, name: 'phone'},
+                                    {width: 768, name: 'tablet'},
+                                    {width: 1279, name: 'desktop'},
+                                    {width: -1, name: 'lgdesktop'},
+                                ]
+
 /*==================/
   TASKS
 /==================*/
-export const image = () =>
+export const image = (o) =>
 {
-    return src(`${imgDIST}/**/*.{jpg,jpeg,png}`)
+    imgSIZES.forEach((size) => {
+        return src(`${imgDIST}/**/*.{jpg,jpeg,png}`)
+        .pipe(imageResize({
+            width: size.width,
+            quality: 1,
+            filter: "Catrom",
+            sharpen: "-1.5x1+0.7+0.02",
+            upscale: false,
+            imageMagick: true
+        }))
+        .pipe(
+            rename((path) => {
+                path.basename += `/${size.name}`;
+            }),
+        )
         .pipe(imagemin([
             imageminPngquant({
                 speed: 1,
@@ -27,4 +50,6 @@ export const image = () =>
             }),
         ]))
         .pipe(dest(imgOUTPUT));
+    })
+    o()
 }
